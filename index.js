251ac0022bf92643dd208fc864bb7c268f1e96f6
Kim Nguyen <kimnivore@gmail.com>
2021-11-02 12:51:09 -0700

    completed task 2

--- index.js
+++ index.js
@@ -89,10 +89,10 @@ Do the following:
  3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-  /*add your code here*/
+function dogYears(age){
+  return age * 7;
 }
-
+console.log('Task 2', dogYears(42));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
