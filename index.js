b78bf568aa3596d5c91667c7463f379a1d7a6f8a
Kim Nguyen <kimnivore@gmail.com>
2021-11-02 17:25:16 -0700

    task 6

--- index.js
+++ index.js
@@ -222,18 +222,13 @@ Using the annoyingSong function below do the following:
     "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
 
-// let bottles = 5;
-// function annoyingSong(bottles){  
-//     for(let i = 5; i >= 0; i--) {
-      
-// console.log("{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall");
-
 function annoyingSong(bottles){  
-    for(let i = bottles; i > 0; i--) {
-        return `${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i - 1}bottles of soda on the wall';
-      }
-    }
-    console.log('Task 6', annoyingSong(5));
+}
+    // for(let i = bottles; i > 0; i--) {
+    //     return `${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i - 1}bottles of soda on the wall';
+    //   }
+    // }
+    // console.log('Task 6', annoyingSong(5));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -248,9 +243,23 @@ Using the grade function below do the following:
  60-69 should return 'you got a D'
  below should return 'you got an F'
 */
-function grade(/*Your Code here */){
-/*Your Code here */
+
+function grade(score){
+ if(score >= 90){
+   return 'you got an A';
+ } else if(score >= 80 && score <= 89); {
+   return 'you got a B';
+ } else if(score >= 70 && score <= 79); {
+   return 'you got a C';
+ } else if(score >= 60 && score <= 69); {
+   return 'you got a D';
+ } else if(score <= 59); {
+   return 'you got an F';
+ }
 }
+
+console.log(grade(100));
+
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
 //Vowel Counter - How many vowels are there?
 /*
